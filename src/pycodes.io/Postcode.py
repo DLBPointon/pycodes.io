5ec9e1930e4e496ba4a843633d912c51d46ab878
DLBPointon <damonlbp@hotmail.co.uk>
2024-05-26 09:02:05 +0100

    Updating code

--- src/pycodes.io/Postcode.py
+++ src/pycodes.io/Postcode.py
@@ -2,23 +2,23 @@ import io
 import requests
 
 class PostcodeObj:
-    def __init__(self, postcode):
+    def __init__(self, postcode, function):
         #  Default URL
-        self.base_url       = "localhost:8000"
-        self.extension      = "/postcodes/"
+        self.base_url       = "192.168.1.227:8070"
+        self.extension      = f"/{function}/"
         self.postcode       = str(postcode)
         code_data           = self.get_data()
-        self.country        = code_data["country"]
-        self.region         = code_data["region"]
-        self.parish         = code_data["parish"]
-        self.quality_rating = code_data["quality"]
+        self.country        = (code_data["country"] if 'country' in code_data else "NA")
+        self.region         = (code_data["region"] if 'region' in code_data else "NA")
+        self.parish         = (code_data["parish"] if 'parish' in code_data else "NA")
+        self.quality_rating = (code_data["quality"] if 'quality' in code_data else "NA")
         self.constituency   = { x: y for x, y in code_data.items() if "parliamentary" in x}
         self.admin          = { x: y for x, y in code_data.items() if "admin" in x}
-        self.inoutcode      = { "outcode": code_data["outcode"], "incode": code_data["incode"]}
+        self.inoutcode      = { "outcode": code_data["outcode"], "incode": (code_data["incode"] if 'incode' in code_data else "NA")}
         self.lat_long       = { "latitude" : code_data["latitude"], "longitude" : code_data["longitude"] }
         self.east_north     = { "eastings" : code_data["eastings"], "northings": code_data["northings"]}
-        self.raw_codes     = code_data["codes"]
-        self.collection = self.__iter__()
+        self.raw_codes      = (code_data["codes"] if 'codes' in code_data else "NA")
+        self.collection     = self.__iter__()
 
     def __iter__(self):
         yield from self.__dict__.items()
@@ -27,25 +27,26 @@ class PostcodeObj:
         txt = io.StringIO()
         txt.write(f"{self.__class__.__name__}:\n")
         [
-            txt.write(f"---- {a}: {b} \n")
+            txt.write(f"- {a}: {b} \n")
             for a, b in self.collection
             if a not in ["block", "collection", "contents"]
         ]
         txt.write(")")
         return txt.getvalue()
-    
+
     def get_data(self):
+        print(f"http://{self.base_url}{self.extension}{self.postcode}")
         return requests.get(f"http://{self.base_url}{self.extension}{self.postcode}").json()["result"]
-    
+
 
 data = {
-    'nhs_ha': 'East of England', 
-    'european_electoral_region': 'Eastern', 
-    'primary_care_trust': 'West Essex', 
-    'lsoa': 'Uttlesford 002C', 
+    'nhs_ha': 'East of England',
+    'european_electoral_region': 'Eastern',
+    'primary_care_trust': 'West Essex',
+    'lsoa': 'Uttlesford 002C',
     'msoa': 'Uttlesford 002',
-    'date_of_introduction': '199507', 
-    'ced': 'Saffron Walden', 
-    'ccg': 'NHS Hertfordshire and West Essex', 
-    'nuts': 'Uttlesford', 
+    'date_of_introduction': '199507',
+    'ced': 'Saffron Walden',
+    'ccg': 'NHS Hertfordshire and West Essex',
+    'nuts': 'Uttlesford',
     'pfa': 'Essex'}
--- src/pycodes.io/main.py
+++ src/pycodes.io/main.py
@@ -1,10 +1,58 @@
+import argparse
+import textwrap
 import requests
 import json
 import sys
 from Postcode import PostcodeObj
 
+
+DESCRIPTION = """
+---------------------------------------
+            PyCodes.io
+---------------------------------------
+           by DLBPointon
+---------------------------------------
+A small wrapper to call API's of:
+    Postcodes.io
+    Doogal
+
+These both serve data on postcodes
+and other related data.
+
+This script has been writted for use
+in decerning which is better for use
+at Citizens advice.
+"""
+
+
+def parse_args():
+  parser = argparse.ArgumentParser(
+      prog="get_READMEs",
+      formatter_class=argparse.RawDescriptionHelpFormatter,
+      description=textwrap.dedent(DESCRIPTION),
+  )
+  parser.add_argument(
+      "postcode", type=str, help="Postcode in format of PE122LN or PE12, depending on function you need."
+  )
+  parser.add_argument(
+      "function", choices=["postcodes","outcodes"], help="api endpoint to use."
+  )
+  return parser.parse_args()
+
+
+def validate_postcode(args):
+    if args.function == "outcodes" and len(args.postcode) <= 4:
+        pass
+    elif args.function == "postcodes" and len(args.postcode) >= 4:
+        pass
+    else:
+        sys.exit("Postcode not correct for function")
+    return args.postcode
+
 def main():
-    print(PostcodeObj(sys.argv[1]))
-    
+    args = parse_args()
+    code = validate_postcode(args)
+    print(PostcodeObj(code, args.function))
+
 if __name__ == "__main__":
     main()
